ed884f93a6953d18d6bc1ee062721044ac4a6860
Venicio <vinicius290371@gmail.com>
2025-11-11 11:57:26 -0300

    added delete and fixed variable bug

--- animal_crud.py
+++ animal_crud.py
@@ -15,7 +15,7 @@ def adicionar_animal():
         pet_comp = str(input("Comportamento do Animal: "))
         pet_date = str(input("Qual a data de chegada do Animal: "))
 
-        conn.execute("INSERT INTO animais(id,nome,idade,raca,especie,saude,comportamento,data) VALUES (?,?,?,?)",(id,pet_name,pet_age,pet_race,pet_especie,pet_saude,pet_comp,pet_date))
+        conn.execute("INSERT INTO animais(nome,idade,raca,especie,saude,comportamento,data) VALUES (?,?,?,?,?,?,?)",(pet_name,pet_age,pet_race,pet_especie,pet_saude,pet_comp,pet_date))
         conn.commit()
         print("Animal adicionado com sucesso")
         return True
@@ -23,3 +23,34 @@ def adicionar_animal():
         print("ERRO: Idade deve ser um número!")
         return False
     
+def leranimais():
+    cur = conn.cursor()
+    cur.execute("SELECT id,nome,idade,raca,especie,saude,comportamento,data FROM animais")
+    animais = cur.fetchall()
+    if not animais:
+        print("Nenhum animal foi cadastrado ainda!")
+        return
+    print("\n=== ANIMAIS REGISTRADOS ===")
+    for animal in animais:
+        print(f"| ID: {animal[0]} | Nome: {animal[1]} | Idade: {animal[2]} | Raça: {animal[3]} | Especie: {animal[4]} | Saúde: {animal[5]} | Comportamento: {animal[6]} | Data: {animal[7]} |")
+def removeranimal():
+    leranimais()
+    cur=conn.cursor()
+    nome_busca= input("\nDigite o nome do animal que deseja remover: ")
+
+    cur.execute("SELECT * FROM animais WHERE nome = ?", (nome_busca,))
+    if not cur.fetchone():
+        print("Animal não encontrado!")
+        return
+    print(f"\nDeseja remover {nome_busca}? dos registros?")
+    print(f"[S]im ou [N]ão")
+    opcao = input(">").upper()
+    if opcao == "S":
+        cur.execute("DELETE FROM animais WHERE nome = ?", (nome_busca,))
+        conn.commit()
+        print("Animal removido!")
+    elif opcao == "N":
+        print("Operação Cancelada!")
+        return
+    else:
+        print("Opção inválida!")
